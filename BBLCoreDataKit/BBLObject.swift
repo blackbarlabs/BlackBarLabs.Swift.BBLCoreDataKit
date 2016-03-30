aa2a4aecee9ca21c7563b823f54c05f4aac2ba62
Joel Perry <joel@joelperry.net>
2016-03-30 16:13:10 -0500

    Formatting

--- BBLCoreDataKit/BBLObject.swift
+++ BBLCoreDataKit/BBLObject.swift
@@ -11,20 +11,20 @@ import CoreData
 
 // MARK: - Protocols
 @objc public protocol BBLObject {
-  var idString: String { get set }
+    var idString: String { get set }
 }
 
 // MARK: - Extensions
 extension NSManagedObject {
-  class var entityName : String {
-    let components = NSStringFromClass(self).componentsSeparatedByString(".")
-    return components[1]
-  }
+    class var entityName : String {
+        let components = NSStringFromClass(self).componentsSeparatedByString(".")
+        return components[1]
+    }
 }
 
 public extension BBLObject {
-  var identifier: NSUUID {
-    get { return NSUUID(UUIDString: idString)! }
-    set { idString = newValue.UUIDString }
-  }
+    var identifier: NSUUID {
+        get { return NSUUID(UUIDString: idString)! }
+        set { idString = newValue.UUIDString }
+    }
 }
