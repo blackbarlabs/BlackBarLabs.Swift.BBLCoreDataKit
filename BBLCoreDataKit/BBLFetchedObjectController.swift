6297ae710f4d4c5792c153fd5ec4b68581a33901
Joel Perry <joel@joelperry.net>
2016-12-10 10:33:39 -0600

    Add BBLFetchedObjectController

---
+++ BBLCoreDataKit/BBLFetchedObjectController.swift
@@ -0,0 +1,70 @@
+//
+//  BBLFetchedObjectController.swift
+//  BBLCoreDataKit
+//
+//  Created by Joel Perry on 12/9/16.
+//  Copyright © 2016 Joel Perry. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public typealias FRC = NSFetchedResultsController<NSFetchRequestResult>
+public typealias FetchedObjectHandler = (BBLObject, FRC) -> Void
+
+public protocol BBLFetchedObjectController: NSFetchedResultsControllerDelegate {
+    associatedtype ControllerStack: BBLStack
+    
+    var stack: ControllerStack { get }
+    var controllers: [FRC] { get set }
+    var objectsInProgress: [ Int : Set<BBLObject> ] { get set }
+    var fetchedObjectHandlers: [ Int : FetchedObjectHandler ] { get set }
+    
+    init(_ stack: ControllerStack)
+}
+
+public extension BBLFetchedObjectController {
+    
+    // MARK: Managed Controllers
+    func addManagedFetchedResultController(_ frc: FRC, fetchedObjectHandler: @escaping FetchedObjectHandler) {
+        if !controllers.contains(frc) { controllers.append(frc) }
+        if objectsInProgress[frc.hash] == nil { objectsInProgress[frc.hash] = Set<BBLObject>() }
+        if fetchedObjectHandlers[frc.hash] == nil { fetchedObjectHandlers[frc.hash] = fetchedObjectHandler }
+    }
+    
+    func removeManagedFetchedResultsController(_ frc: FRC) {
+        if let index = controllers.index(of: frc) { controllers.remove(at: index) }
+        if objectsInProgress[frc.hash] != nil { objectsInProgress[frc.hash] = nil }
+        if fetchedObjectHandlers[frc.hash] != nil { fetchedObjectHandlers[frc.hash] = nil }
+    }
+    
+    func startManagedControllers() {
+        controllers.forEach {
+            $0.delegate = self
+            try! $0.performFetch()
+            controllerDidChangeContent?($0)
+        }
+    }
+    
+    func stopManagedControllers() {
+        controllers.removeAll()
+        objectsInProgress.removeAll()
+        fetchedObjectHandlers.removeAll()
+    }
+    
+    // MARK: - Progress
+    func setObject(_ object: BBLObject, inProgress: Bool, onFetchedResultsController frc: FRC) {
+        guard var progressSet = objectsInProgress[frc.hash] else { return }
+        if inProgress {
+            progressSet.insert(object)
+        } else {
+            progressSet.remove(object)
+        }
+        objectsInProgress[frc.hash] = progressSet
+    }
+    
+    func objectIsInProgress(_ object: BBLObject, onFetchedResultsController frc: FRC) -> Bool {
+        guard let progressSet = objectsInProgress[frc.hash] else { return false }
+        return progressSet.contains(object)
+    }
+}
