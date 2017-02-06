b9b38a8e27b338e95165224e7003af2051c8b5ee
Joel Perry <joel@joelperry.net>
2017-02-06 13:45:19 -0600

    Add touchRelationships()

--- BBLCoreDataKit/BBLObject.swift
+++ BBLCoreDataKit/BBLObject.swift
@@ -26,6 +26,24 @@ open class BBLObject: NSManagedObject {
             self.didChangeValue(forKey: "idString")
         }
     }
+    
+    open func touchRelationships() {
+        entity.relationshipsByName.forEach { (key, relationship) in
+            if let inverse = relationship.inverseRelationship {
+                if relationship.isToMany, let set = self.value(forKey: relationship.name) as? Set<BBLObject> {
+                    set.forEach {
+                        $0.willChangeValue(forKey: inverse.name)
+                        $0.didChangeValue(forKey: inverse.name)
+                    }
+                }
+                
+                if !relationship.isToMany, let object = self.value(forKey: relationship.name) as? BBLObject {
+                    object.willChangeValue(forKey: inverse.name)
+                    object.didChangeValue(forKey: inverse.name)
+                }
+            }
+        }
+    }
 }
 
 // MARK: - Extensions
