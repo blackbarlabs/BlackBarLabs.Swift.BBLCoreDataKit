eeaa107112490880ed63491742f23d1d0d6bd2d7
Joel Perry <joel@joelperry.net>
2017-03-18 15:10:46 -0500

    Add kvoHandler to BBLObject

--- BBLCoreDataKit/BBLObject.swift
+++ BBLCoreDataKit/BBLObject.swift
@@ -15,15 +15,15 @@ open class BBLObject: NSManagedObject {
     
     open var identifier: UUID {
         get {
-            self.willAccessValue(forKey: "idString")
-            let string = self.primitiveValue(forKey: "idString") as! String
-            self.didAccessValue(forKey: "idString")
+            self.willAccessValue(forKey: #keyPath(idString))
+            let string = self.primitiveValue(forKey: #keyPath(idString)) as! String
+            self.didAccessValue(forKey: #keyPath(idString))
             return UUID(uuidString: string)!
         }
         set {
-            self.willChangeValue(forKey: "idString")
-            self.setPrimitiveValue(newValue.uuidString, forKey: "idString")
-            self.didChangeValue(forKey: "idString")
+            self.willChangeValue(forKey: #keyPath(idString))
+            self.setPrimitiveValue(newValue.uuidString, forKey: #keyPath(idString))
+            self.didChangeValue(forKey: #keyPath(idString))
         }
     }
     
@@ -43,6 +43,69 @@ open class BBLObject: NSManagedObject {
                 }
             }
         }
+    }
+    
+    // MARK: - KVO
+    private var kvoContext = 0
+    private var kvoHandlers = [String : (new: Any?, old: Any?) -> Void]()
+    private var observedKeys = Set<String>()
+    private func isObserving(_ key: String) -> Bool { return observedKeys.contains(key) }
+    
+    open func addKvoHandler(forKey key: String,
+                            handler: @escaping (_ new: Any?, _ old: Any?) -> Void) {
+        kvoHandlers[key] = handler
+    }
+    
+    open func removeKvoHandler(forKey key: String) {
+        if isObserving(key) { removeObserver(self, forKeyPath: key, context: &kvoContext) }
+        kvoHandlers.removeValue(forKey: key)
+    }
+    
+    override open func awakeFromFetch() {
+        kvoHandlers.forEach {
+            if !isObserving($0.key) {
+                addObserver(self, forKeyPath: $0.key, options: [ .new, .old ], context: &kvoContext)
+                observedKeys.insert($0.key)
+            }
+        }
+    }
+    
+    override open func awakeFromInsert() {
+        kvoHandlers.forEach {
+            if !isObserving($0.key) {
+                addObserver(self, forKeyPath: $0.key, options: [ .new, .old ], context: &kvoContext)
+                observedKeys.insert($0.key)
+            }
+        }
+    }
+    
+    override open func prepareForDeletion() {
+        kvoHandlers.forEach {
+            if isObserving($0.key) {
+                removeObserver(self, forKeyPath: $0.key, context: &kvoContext)
+                observedKeys.remove($0.key)
+            }
+        }
+        kvoHandlers.removeAll()
+    }
+    
+    override open func willTurnIntoFault() {
+        kvoHandlers.forEach {
+            if isObserving($0.key) {
+                removeObserver(self, forKeyPath: $0.key, context: &kvoContext)
+                observedKeys.remove($0.key)
+            }
+        }
+    }
+    
+    override open func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard context == &kvoContext else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+        
+        guard let keyPath = keyPath, let handler = kvoHandlers[keyPath] else { return }
+        handler(change?[.newKey], change?[.oldKey])
     }
 }
 
